74f8649e1727fbe320e64748b5357535a43636e7
Lika380 <lika.kaslandziya@gmail.com>
2025-08-05 17:16:42 +0300

    Fix path to frontend build

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,1 @@
-export const API_BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3001'
-    : 'https://apsadep.onrender.com'; // если используешь прод
+ export const API_BASE_URL = 'https://apsadepserver.onrender.com';// если используешь прод
