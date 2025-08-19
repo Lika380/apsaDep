21f1a6d03673bcda4201f6161898e479fc689c90
Lika380 <lika.kaslandziya@gmail.com>
2025-08-19 20:33:22 +0300

    Build frontend and integrate with server

--- client/src/dist/dist/vite.config.dev.js
+++ client/src/dist/dist/vite.config.dev.js
@@ -1,8 +1,12 @@
 "use strict";
+
 exports.__esModule = true;
+
 var vite_1 = require("vite");
+
 var plugin_react_1 = require("@vitejs/plugin-react");
+
 exports["default"] = vite_1.defineConfig({
-    base: '/',
-    plugins: [plugin_react_1["default"]()]
+  base: '/',
+  plugins: [plugin_react_1["default"]()]
 });
